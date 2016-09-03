c6dd46e82b0ba0017c1693e1bc2568692937f759
kurtis-s <kurtis_s@hotmail.com>
2016-09-03 19:56:59 -0400

    added a bit of code for conversion to utm

--- data/transforms/building_assignment.R
+++ data/transforms/building_assignment.R
@@ -1,9 +1,23 @@
 rm(list=ls())
 
+library(sp)
+library(rgdal)
 library(RANN)
 
 source("utilities.R")
 
+
+# Convert to UTM ----------------------------------------------------------
+
+xy <- latlon_parcels %>% select(lat, lon) %>% mutate(lon=lon, lat=lat)
+coordinates(xy) <- c("lon", "lat")
+proj4string(xy) <- CRS("+proj=longlat +datum=WGS84")  ## for example
+
+res <- spTransform(xy, CRS("+proj=utm +zone=17 ellps=WGS84"))
+res
+plot(res@coords[,1], res@coords[,2])
+
+# Nearest neighbors -------------------------------------------------------
 dat_parcels_transform <- read_csv(DATA_TRANSFORM_BASE_PATH, "dat_parcels_transform.csv")
 read_transformed_dat(environment())
 
@@ -13,3 +27,5 @@ latlon_crime <- dat_crime_transform %>% select(lat, lon)
 
 x <- nn2(latlon_parcels, latlon_311, k=1)
 x <- nn2(latlon_parcels, latlon_crime, k=1)
+
+
