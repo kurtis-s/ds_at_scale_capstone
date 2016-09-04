5d3109c7e999afd46fbfcfe0077ec2e81d77a4b9
kurtis-s <kurtis_s@hotmail.com>
2016-09-04 10:17:34 -0400

    added some code to specify a max building distance

--- data/transforms/building_assignment.R
+++ data/transforms/building_assignment.R
@@ -17,10 +17,14 @@ building_coords <- datenv$dat_parcels_transform %>% select(x, y)
 assign_building_id <- function(dat) {
     coords <- dat %>% select(x, y)
     nearest <- nn2(building_coords, coords, k=1)
-    # nearest$nn.dists might want to do something with this later like if distance
-    # is too large don't assign to that building
 
-    dat$BuildID <- datenv$dat_parcels_transform$ID[nearest$nn.idx]
+    # A observation is "too far" away if it is further than max_dist from the
+    # building's center
+    max_dist <- 30 # 30 meters ~ 100 feet
+    too_far <- ifelse(nearest$nn.dists > max_dist, TRUE, FALSE)
+
+    nearest_building_IDs <- datenv$dat_parcels_transform$ID[nearest$nn.idx]
+    dat$BuildID <- ifelse(too_far, NA, nearest_building_IDs)
 
     return(dat)
 }
